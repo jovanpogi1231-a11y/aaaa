f9384310f36a9b954292d6c34fee22b83fb4f5cd
jovanpogi1231-a11y <jovanpogi1231@gmail.com>
2026-05-09 17:51:50 +0800

    Update Parry_Animation.lua

--- Parry_Animation.lua
+++ Parry_Animation.lua
@@ -1,12 +1,10 @@
-local Parry_Animation_API = {}
- 
 local Players = game:GetService('Players')
-local ReplicatedStorage = game:GetService('ReplicatedStorage')
 local Player = Players.LocalPlayer
  
+local Auto_Parry = {}
 local Grab_Parry = nil
  
-function Parry_Animation_API.Play_Animation()
+function Auto_Parry.Parry_Animation()
     local Character = Player.Character
     if not Character then return end
  
@@ -16,21 +14,23 @@ function Parry_Animation_API.Play_Animation()
     local Animator = Humanoid:FindFirstChildOfClass('Animator')
     if not Animator then return end
  
-    local Parry_Animation = ReplicatedStorage.Shared.SwordAPI.Collection.Default:FindFirstChild('GrabParry')
+    local Parry_Animation = game:GetService('ReplicatedStorage').Shared.SwordAPI.Collection.Default:FindFirstChild('GrabParry')
     local Current_Sword = Character:GetAttribute('CurrentlyEquippedSword')
- 
+        
     if not Current_Sword then return end
+        
     if not Parry_Animation then return end
  
-    local Sword_Data = ReplicatedStorage.Shared.ReplicatedInstances.Swords.GetSword:Invoke(Current_Sword)
+    local Sword_Data = game:GetService('ReplicatedStorage').Shared.ReplicatedInstances.Swords.GetSword:Invoke(Current_Sword)
     if not Sword_Data or not Sword_Data['AnimationType'] then return end
  
-    for _, object in ReplicatedStorage.Shared.SwordAPI.Collection:GetChildren() do
+    
+    for _, object in game:GetService('ReplicatedStorage').Shared.SwordAPI.Collection:GetChildren() do
         if object.Name == Sword_Data['AnimationType'] then
             if object:FindFirstChild('GrabParry') or object:FindFirstChild('Grab') then
                 local sword_animation_type = 'GrabParry'
                 if object:FindFirstChild('Grab') then
-                    sword_animation_type = 'Grab'
+                        sword_animation_type = 'Grab'
                 end
                 Parry_Animation = object[sword_animation_type]
                 break
@@ -38,6 +38,7 @@ function Parry_Animation_API.Play_Animation()
         end
     end
  
+        
     local Success, Track = pcall(function()
         return Animator:LoadAnimation(Parry_Animation)
     end)
@@ -51,14 +52,4 @@ function Parry_Animation_API.Play_Animation()
     end
 end
  
-function Parry_Animation_API.Stop_Animation()
-    if Grab_Parry and Grab_Parry.IsPlaying then
-        Grab_Parry:Stop(0)
-    end
-end
- 
-function Parry_Animation_API.Get_Track()
-    return Grab_Parry
-end
- 
-return Parry_Animation_API
+return Auto_Parry
